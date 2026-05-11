f3612456b58afd5c3f5002c56c51ca68b0fcc452
Demarbiaz <damarbiazodevane@abbey.bac.edu>
2026-05-11 01:15:56 -0400

    Connected Giphy API and fetched gif data

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
-console.log("script.js loaded");
+const apiKey = VjxnLzi4JDl2AGOMBGBrFoVMbkss0Lfe;
+
+const gifContainer = document.querySelector("#gif-container");
+const fetchBtn = document.querySelector("#fetch-gif-btn");
+const searchInput = document.querySelector("#search-input");
+
+fetchBtn.addEventListener("click", async () => {
+
+  const searchTerm = searchInput.value;
+
+  gifContainer.innerHTML = "";
+
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=12&rating=g`;
+
+  try {
+
+    
+    const response = await fetch(endpoint);
+
+    const data = await response.json();
+
+    const images = data.data.map((gif) => {
+      return gif.images.original.url;
+    });
+
+    console.log(images);
+
+    images.forEach((imageUrl) => {
+      gifContainer.innerHTML += `
+        <div class="col-3 mb-3">
+          <img src="${imageUrl}" class="img-fluid">
+        </div>
+      `;
+    });
+
+  } catch (error) {
+    console.log("Error fetching gifs:", error);
+  }
+
+});
